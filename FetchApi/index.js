4e2176b6e1a37661d42cb82532597d989e9c7853
Sanskar Pandey <sanskar2416@gmail.com>
2026-09-01 10:04:19 +0530

    fetch api learned

--- FetchApi/index.js
+++ FetchApi/index.js
@@ -1,7 +1,38 @@
 
 const root=document.getElementById('container');
 const button=document.getElementById('btn');
-function fetchdata(){
-    alert("hii");
+const disp=document.getElementById('disp');
+const loader=document.getElementById('loader');
+async function fetchdata(){
+    //alert("hii");
+    
+
+    try{
+        loader.innerHTML='<h2 style= color:red> loading data....<h2>'
+        const serverdata= await fetch('https://fakestoreapi.com/products');
+                const jsondata=await serverdata.json();
+                //console.log(jsondata[0].title);
+                //disp.innerHTML=`${jsondata[0].title}`;
+                let table=`<table border=2px>
+                ${
+                    jsondata.map((ele)=>(
+                        `<tr>
+                        <td><img src=${ele.image} height=200 width=200></td>
+                        <td>${ele.title}</td>
+                        <td>${ele.description}</td>
+                        <td>${ele.price}</td>
+                        <td>${ele.category}</td>
+                        </tr>`
+                    ))
+                }
+
+                </table>`
+                disp.innerHTML=table;
+    }catch(e){
+        console.log("Error is"+e)
+    }
+    finally{
+        loader.innerHTML='';
+    }
 }
 button.addEventListener('click',fetchdata);
